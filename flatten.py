8d19e804b4e544eff48d11546468882544666653
Adrian Göransson <adrian@adriang.se>
2021-03-27 12:10:35 +0100

    Implement the flatten function

--- flatten.py
+++ flatten.py
@@ -4,8 +4,24 @@ import json
 from typing import Dict, List
 
 
+def _flatten(data: Dict, prefix: List[str]) -> Dict:
+    flattened = {}
+
+    for (key, value) in data.items():
+        prefixes = prefix + [key]
+
+        if isinstance(value, dict):
+            x = _flatten(value, prefixes)
+            flattened.update(x)
+        else:
+            prefix_key = ".".join(prefixes)
+            flattened[prefix_key] = value
+
+    return flattened
+
+
 def flatten(data: Dict) -> Dict:
-    return data
+    return _flatten(data, [])
 
 
 if __name__ == "__main__":
